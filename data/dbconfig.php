53fb251088282c3cb03e36109fd9a0e81e328ad5
Sefat Noor Orni <sefnoor@gmail.com>
2017-01-19 09:19:59 +0600

    Update dbconfig.php

--- data/dbconfig.php
+++ data/dbconfig.php
@@ -1,1 +1,16 @@
+<?php
+function get_db_connection()
+{
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "file_formatter_db";
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 
+}
+return $conn;
+
+}
+?>
